e86777674df74ef7affba5dd153f630860fc8a0d
Hassan Al-Shammari <hsn999hsn@gmail.com>
2021-12-01 13:59:03 +0000

    a

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let namesArray: [String] =
+    ["Hassan", "Amer", "Hamza"," Tariq", "Sati",
+    "Danny", "Kevin", "Hussain", "Ahmed", "Mohammed"]
+    
+    @IBOutlet weak var nameLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameLabel.text = namesArray[0]
     }
 
+    @IBAction func coldCallButton(_ sender: UIButton) {
+        changeName()
+    }
+    
+    private func changeName(){
+        nameLabel.text = namesArray[getRandomNumber()]
+    }
+    
+    private func getRandomNumber() -> Int{
+        return Int.random(in: 0..<10)
+    }
 
 }
 
